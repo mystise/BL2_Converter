fb757817f8bc88d79764ae942f20b9cbcfa9fedf
mystise <246232+mystise@users.noreply.github.com>
2017-04-06 16:45:53 -0600

    Multi-file version

--- src/main.rs
+++ src/main.rs
@@ -1,70 +1,159 @@
 use std::env;
 use std::fs::File;
+use std::path::{Path, PathBuf};
 use std::io::{BufRead, BufReader};
-use std::process;
 
 fn print_help() {
     println!("-h : help");
-    println!("-p : package (first column)");
-    println!("-f : file to read");
-    println!("-s : start value for keys");
+    println!("-f : files to read, comma separated list");
+}
+
+enum HotfixType {
+    OnDemand(Option<String>),
+    Level(Option<String>),
+    Patch
 }
 
 fn main() {
-    let mut package = None;
-    let mut start = 0;
-    let mut file = None;
+    let mut files = None;
     for arg in env::args() {
         let arg = arg.split('=').collect::<Vec<&str>>();
         match arg[0] {
             "-h" => { // Help
                 print_help();
-                process::exit(0);
+                return;
             },
-            "-p" => { // Package
-                package = Some(arg[1].to_string());
-            },
-            "-f" => { // File
-                file = Some(File::open(arg[1]).unwrap());
-            },
-            "-s" => { // Start value
-                start = arg[1].parse().unwrap();
+            "-f" => { // Files
+                files = Some(arg[1].split(',').map(|x| (Path::new(x).to_path_buf(), File::open(x).unwrap())).collect::<Vec<(PathBuf, File)>>());
             },
             _ => {
             }
         }
     }
-    if let None = file {
+    if let None = files {
         print_help();
-        process::exit(0);
+        return;
     }
-    if let None = package {
+    let files = files.unwrap();
+    if files.len() == 0 {
         print_help();
-        process::exit(0);
+        return;
     }
-    let package = package.unwrap();
-    let file = BufReader::new(file.unwrap());
-    let mut count = 0;
-    for line in file.lines().filter_map(|result| result.ok()) {
-        match line.chars().nth(0) {
-            Some('#') | Some('\n') | Some('\r') | None => {
-                continue;
-            },
-            _ => {
+
+    let mut keys = Vec::new();
+    let mut values = Vec::new();
+
+    let mut on_demand_index = 1;
+    let mut level_index = 1;
+    let mut patch_index = 1;
+
+    let mut current_type = HotfixType::Patch;
+
+    for file in files {
+        let prefix = file.0.file_name().unwrap().to_str().unwrap().splitn(2, ".").next().unwrap();
+        let file = BufReader::new(file.1);
+
+        for line in file.lines().filter_map(|result| result.ok()) {
+            match line.chars().nth(0) {
+                Some('#') | Some('\n') | Some('\r') | None => {
+                    continue;
+                },
+                _ => {
+                }
+            }
+            let mut parts = line.splitn(2, " ");
+            let command = parts.next().unwrap().to_lowercase();
+            let data = parts.next().unwrap();
+            match command.as_str() {
+                "set" => {
+                    let mut parts = data.splitn(3, " ");
+                    match &current_type {
+                        &HotfixType::Level(ref x) => {
+                            keys.push(format!("SparkLevelPatchEntry-{}{}", prefix, level_index));
+                            values.push(format!("{},{},{},,{}", x.clone().unwrap_or("".to_string()), parts.next().unwrap(), parts.next().unwrap(), parts.next().unwrap()));
+                            level_index += 1;
+                        },
+                        &HotfixType::OnDemand(ref x) => {
+                            keys.push(format!("SparkOnDemandPatchEntry-{}{}", prefix, on_demand_index));
+                            values.push(format!("{},{},{},,{}", x.clone().unwrap_or("".to_string()), parts.next().unwrap(), parts.next().unwrap(), parts.next().unwrap()));
+                            on_demand_index += 1;
+                        },
+                        &HotfixType::Patch => {
+                            keys.push(format!("SparkPatchEntry-{}{}", prefix, patch_index));
+                            values.push(format!("{},{},,{}", parts.next().unwrap(), parts.next().unwrap(), parts.next().unwrap()));
+                            patch_index += 1;
+                        }
+                    }
+                },
+                "set_cmp" => {
+                    let mut parts = data.splitn(4, " ");
+                    match &current_type {
+                        &HotfixType::Level(ref x) => {
+                            keys.push(format!("SparkLevelPatchEntry-{}{}", prefix, level_index));
+                            values.push(format!("{},{},{},{},{}", x.clone().unwrap_or("".to_string()), parts.next().unwrap(), parts.next().unwrap(), parts.next().unwrap(), parts.next().unwrap()));
+                            level_index += 1;
+                        },
+                        &HotfixType::OnDemand(ref x) => {
+                            keys.push(format!("SparkOnDemandPatchEntry-{}{}", prefix, on_demand_index));
+                            values.push(format!("{},{},{},{},{}", x.clone().unwrap_or("".to_string()), parts.next().unwrap(), parts.next().unwrap(), parts.next().unwrap(), parts.next().unwrap()));
+                            on_demand_index += 1;
+                        },
+                        &HotfixType::Patch => {
+                            keys.push(format!("SparkPatchEntry-{}{}", prefix, patch_index));
+                            values.push(format!("{},{},{},{}", parts.next().unwrap(), parts.next().unwrap(), parts.next().unwrap(), parts.next().unwrap()));
+                            patch_index += 1;
+                        }
+                    }
+                },
+                "start" => {
+                    // Level, OnDemand, and Patch
+                    let mut parts = data.splitn(2, " ");
+                    let command = parts.next().unwrap().to_lowercase();
+                    match command.as_str() {
+                        "ondemand" => {
+                            let package = parts.next().unwrap();
+                            current_type = HotfixType::OnDemand(if package.to_lowercase() == "none" {
+                                None
+                            } else {
+                                Some(package.to_string())
+                            });
+                        },
+                        "level" => {
+                            let package = parts.next().unwrap();
+                            current_type = HotfixType::Level(if package.to_lowercase() == "none" {
+                                None
+                            } else {
+                                Some(package.to_string())
+                            });
+                        },
+                        "patch" => {
+                            current_type = HotfixType::Patch;
+                        },
+                        _ => {
+                        }
+                    }
+                },
+                _ => {
+                }
             }
         }
-        count += 1;
-        let mut parts = line.splitn(4, " ");
-        let _ = parts.next().unwrap(); // 'set'
-        let path = parts.next().unwrap();
-        let key = parts.next().unwrap();
-        let val = parts.next().unwrap();
-        print!(",\"{},{},{},,{}\"", package, path, key, val);
     }
-    println!("");
-    println!("");
-    for i in 0..count {
-        print!(",\"SparkOnDemandPatchEntry{}\"", i + start);
+    print!("set Transient.SparkServiceConfiguration_6 Keys (");
+    for i in 0..keys.len() {
+        if i != 0 {
+            print!(",");
+        }
+        print!("\"{}\"", keys[i]);
     }
-    println!("");
+    println!(")\r");
+    println!("\r");
+    println!("\r");
+    print!("set Transient.SparkServiceConfiguration_6 Values (");
+    for i in 0..values.len() {
+        if i != 0 {
+            print!(",");
+        }
+        print!("\"{}\"", values[i]);
+    }
+    println!(")\r");
 }
