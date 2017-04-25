47ceb3001e0d95fc1db334aca78e7792cc7e6a43
mystise <246232+mystise@users.noreply.github.com>
2017-04-24 18:37:08 -0600

    Refactored parsing

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,14 @@ fn print_help() {
     println!("-f : files to read, comma separated list");
 }
 
+#[derive(Clone)]
+enum ParsedLine {
+    Set(String, String, String), // Object, path, value
+    SetCmp(String, String, String, String), // Object, path, compare value, value
+    Start(HotfixType)
+}
+
+#[derive(Clone)]
 enum HotfixType {
     OnDemand(Option<String>),
     Level(Option<String>),
@@ -15,6 +23,9 @@ enum HotfixType {
 }
 
 fn main() {
+    // Run in folder
+    // Run on folder
+    // Run on files
     let mut files = None;
     for arg in env::args() {
         let arg = arg.split('=').collect::<Vec<&str>>();
@@ -40,17 +51,10 @@ fn main() {
         return;
     }
 
-    let mut keys = Vec::new();
-    let mut values = Vec::new();
-
-    let mut on_demand_index = 1;
-    let mut level_index = 1;
-    let mut patch_index = 1;
-
-    let mut current_type = HotfixType::Patch;
+    let mut lines = Vec::new();
 
     for file in files {
-        let prefix = file.0.file_name().expect("File name not found").to_str().expect("File name not unicode").splitn(2, ".").next().expect("File has no name");
+        let prefix = file.0.file_name().expect("File name not found").to_str().expect("File name not unicode").splitn(2, ".").next().expect("File has no name").to_string();
         let file = BufReader::new(file.1);
 
         for line in file.lines().filter_map(|result| result.ok()) {
@@ -67,52 +71,21 @@ fn main() {
             match command.as_str() {
                 "set" => {
                     let mut parts = data.splitn(3, " ");
-                    match &current_type {
-                        &HotfixType::Level(ref x) => {
-                            keys.push(format!("SparkLevelPatchEntry-{}{}", prefix, level_index));
-                            values.push(format!("{},{},{},,{}", x.clone().unwrap_or("".to_string()), parts.next().expect(&format!("Syntax error: {}", line)), parts.next().expect(&format!("Syntax error: {}", line)), parts.next().expect(&format!("Syntax error: {}", line))));
-                            level_index += 1;
-                        },
-                        &HotfixType::OnDemand(ref x) => {
-                            keys.push(format!("SparkOnDemandPatchEntry-{}{}", prefix, on_demand_index));
-                            values.push(format!("{},{},{},,{}", x.clone().unwrap_or("".to_string()), parts.next().expect(&format!("Syntax error: {}", line)), parts.next().expect(&format!("Syntax error: {}", line)), parts.next().expect(&format!("Syntax error: {}", line))));
-                            on_demand_index += 1;
-                        },
-                        &HotfixType::Patch => {
-                            keys.push(format!("SparkPatchEntry-{}{}", prefix, patch_index));
-                            values.push(format!("{},{},,{}", parts.next().expect(&format!("Syntax error: {}", line)), parts.next().expect(&format!("Syntax error: {}", line)), parts.next().expect(&format!("Syntax error: {}", line))));
-                            patch_index += 1;
-                        }
-                    }
+                    lines.push((prefix.clone(), ParsedLine::Set(parts.next().expect(&format!("Syntax error: {}", line)).to_string(), parts.next().expect(&format!("Syntax error: {}", line)).to_string(), parts.next().expect(&format!("Syntax error: {}", line)).to_string())));
                 },
                 "set_cmp" => {
                     let mut parts = data.splitn(4, " ");
-                    match &current_type {
-                        &HotfixType::Level(ref x) => {
-                            keys.push(format!("SparkLevelPatchEntry-{}{}", prefix, level_index));
-                            values.push(format!("{},{},{},{},{}", x.clone().unwrap_or("".to_string()), parts.next().expect(&format!("Syntax error: {}", line)), parts.next().expect(&format!("Syntax error: {}", line)), parts.next().expect(&format!("Syntax error: {}", line)), parts.next().expect(&format!("Syntax error: {}", line))));
-                            level_index += 1;
-                        },
-                        &HotfixType::OnDemand(ref x) => {
-                            keys.push(format!("SparkOnDemandPatchEntry-{}{}", prefix, on_demand_index));
-                            values.push(format!("{},{},{},{},{}", x.clone().unwrap_or("".to_string()), parts.next().expect(&format!("Syntax error: {}", line)), parts.next().expect(&format!("Syntax error: {}", line)), parts.next().expect(&format!("Syntax error: {}", line)), parts.next().expect(&format!("Syntax error: {}", line))));
-                            on_demand_index += 1;
-                        },
-                        &HotfixType::Patch => {
-                            keys.push(format!("SparkPatchEntry-{}{}", prefix, patch_index));
-                            values.push(format!("{},{},{},{}", parts.next().expect(&format!("Syntax error: {}", line)), parts.next().expect(&format!("Syntax error: {}", line)), parts.next().expect(&format!("Syntax error: {}", line)), parts.next().expect(&format!("Syntax error: {}", line))));
-                            patch_index += 1;
-                        }
-                    }
+                    lines.push((prefix.clone(), ParsedLine::SetCmp(parts.next().expect(&format!("Syntax error: {}", line)).to_string(), parts.next().expect(&format!("Syntax error: {}", line)).to_string(), parts.next().expect(&format!("Syntax error: {}", line)).to_string(), parts.next().expect(&format!("Syntax error: {}", line)).to_string())));
                 },
                 "start" => {
                     // Level, OnDemand, and Patch
                     let mut parts = data.splitn(2, " ");
                     let command = parts.next().expect(&format!("Syntax error: {}", line)).to_lowercase();
+                    let mut hotfix_type = HotfixType::Patch;
                     match command.as_str() {
                         "ondemand" => {
                             let package = parts.next().expect(&format!("Syntax error: {}", line));
-                            current_type = HotfixType::OnDemand(if package.to_lowercase() == "none" {
+                            hotfix_type = HotfixType::OnDemand(if package.to_lowercase() == "none" {
                                 None
                             } else {
                                 Some(package.to_string())
@@ -120,24 +93,78 @@ fn main() {
                         },
                         "level" => {
                             let package = parts.next().expect(&format!("Syntax error: {}", line));
-                            current_type = HotfixType::Level(if package.to_lowercase() == "none" {
+                            hotfix_type = HotfixType::Level(if package.to_lowercase() == "none" {
                                 None
                             } else {
                                 Some(package.to_string())
                             });
                         },
-                        "patch" => {
-                            current_type = HotfixType::Patch;
-                        },
                         _ => {
                         }
                     }
+                    lines.push(("".to_string(), ParsedLine::Start(hotfix_type)))
                 },
                 _ => {
                 }
             }
         }
     }
+
+    let mut keys = Vec::new();
+    let mut values = Vec::new();
+
+    let mut current_type = HotfixType::Patch;
+
+    let mut on_demand_index = 1;
+    let mut level_index = 1;
+    let mut patch_index = 1;
+
+    for parsed_line in lines {
+        match parsed_line {
+            (prefix, ParsedLine::Set(obj, path, value)) => {
+                match &current_type {
+                    &HotfixType::Level(ref x) => {
+                        keys.push(format!("SparkLevelPatchEntry-{}{}", prefix, level_index));
+                        values.push(format!("{},{},{},,{}", x.clone().unwrap_or("".to_string()), obj, path, value));
+                        level_index += 1;
+                    },
+                    &HotfixType::OnDemand(ref x) => {
+                        keys.push(format!("SparkOnDemandPatchEntry-{}{}", prefix, on_demand_index));
+                        values.push(format!("{},{},{},,{}", x.clone().unwrap_or("".to_string()), obj, path, value));
+                        on_demand_index += 1;
+                    },
+                    &HotfixType::Patch => {
+                        keys.push(format!("SparkPatchEntry-{}{}", prefix, patch_index));
+                        values.push(format!("{},{},,{}", obj, path, value));
+                        patch_index += 1;
+                    }
+                }
+            },
+            (prefix, ParsedLine::SetCmp(obj, path, cmp_value, value)) => {
+                match &current_type {
+                    &HotfixType::Level(ref x) => {
+                        keys.push(format!("SparkLevelPatchEntry-{}{}", prefix, level_index));
+                        values.push(format!("{},{},{},{},{}", x.clone().unwrap_or("".to_string()), obj, path, cmp_value, value));
+                        level_index += 1;
+                    },
+                    &HotfixType::OnDemand(ref x) => {
+                        keys.push(format!("SparkOnDemandPatchEntry-{}{}", prefix, on_demand_index));
+                        values.push(format!("{},{},{},{},{}", x.clone().unwrap_or("".to_string()), obj, path, cmp_value, value));
+                        on_demand_index += 1;
+                    },
+                    &HotfixType::Patch => {
+                        keys.push(format!("SparkPatchEntry-{}{}", prefix, patch_index));
+                        values.push(format!("{},{},{},{}", obj, path, cmp_value, value));
+                        patch_index += 1;
+                    }
+                }
+            },
+            (_, ParsedLine::Start(hotfix_type)) => {
+                current_type = hotfix_type;
+            }
+        }
+    }
+
     print!("set Transient.SparkServiceConfiguration_6 Keys (");
     for i in 0..keys.len() {
         if i != 0 {
@@ -145,9 +172,9 @@ fn main() {
         }
         print!("\"{}\"", keys[i]);
     }
-    println!(")\r");
-    println!("\r");
-    println!("\r");
+    println!(")");
+    println!("");
+    println!("");
     print!("set Transient.SparkServiceConfiguration_6 Values (");
     for i in 0..values.len() {
         if i != 0 {
@@ -155,5 +182,5 @@ fn main() {
         }
         print!("\"{}\"", values[i]);
     }
-    println!(")\r");
+    println!(")");
 }
